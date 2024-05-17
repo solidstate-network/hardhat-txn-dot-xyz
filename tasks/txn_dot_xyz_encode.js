54a1dae5d40139ac54bf4220a37d1ef83487a2f7
Nick Barry <itsnickbarry@protonmail.ch>
2024-05-17 14:21:21 +0400

    apply prettier formatting

--- tasks/txn_dot_xyz_encode.js
+++ tasks/txn_dot_xyz_encode.js
@@ -4,36 +4,38 @@ const { stringifyUrl } = require('query-string');
 
 const API_ENDPOINT = 'https://txn.xyz/v0/decode/';
 
-subtask(
-  'txn-dot-xyz-encode'
-).addOptionalParam(
-  'chainId', 'Target chain ID', undefined, types.int
-).addParam(
-  'contractAddress', 'Target address', undefined, types.string
-).addOptionalParam(
-  'fn', 'Target function name', undefined, types.string
-).addOptionalParam(
-  'fnParams', 'Target function call arguments', [], types.json
-).setAction(async function (args, hre) {
-  if (!Array.isArray(args.fnParams)) {
-    throw new HardhatPluginError('fnParams must be array');
-  }
+subtask('txn-dot-xyz-encode')
+  .addOptionalParam('chainId', 'Target chain ID', undefined, types.int)
+  .addParam('contractAddress', 'Target address', undefined, types.string)
+  .addOptionalParam('fn', 'Target function name', undefined, types.string)
+  .addOptionalParam(
+    'fnParams',
+    'Target function call arguments',
+    [],
+    types.json,
+  )
+  .setAction(async function (args, hre) {
+    if (!Array.isArray(args.fnParams)) {
+      throw new HardhatPluginError('fnParams must be array');
+    }
 
-  const query = {
-    contractAddress: args.contractAddress,
-    fn: args.fn,
-  };
+    const query = {
+      contractAddress: args.contractAddress,
+      fn: args.fn,
+    };
 
-  // note case change in variable name (chainId => chainID)
-  if (args.chainId) {
-    query.chainID = args.chainId;
-  } else {
-    query.chainID = parseInt(await hre.network.provider.send('eth_chainId'));
-  }
+    // note case change in variable name (chainId => chainID)
+    if (args.chainId) {
+      query.chainID = args.chainId;
+    } else {
+      query.chainID = parseInt(await hre.network.provider.send('eth_chainId'));
+    }
 
-  if (args.fnParams.length) {
-    query.fnParams = args.fnParams.map((arg, index) => `${ index }=${ arg }`).join(',');
-  }
+    if (args.fnParams.length) {
+      query.fnParams = args.fnParams
+        .map((arg, index) => `${index}=${arg}`)
+        .join(',');
+    }
 
-  return stringifyUrl({ url: API_ENDPOINT, query });
-});
+    return stringifyUrl({ url: API_ENDPOINT, query });
+  });
--- tasks/txn_dot_xyz_send.js
+++ tasks/txn_dot_xyz_send.js
@@ -6,42 +6,51 @@ const readline = require('readline');
 const API_ENDPOINT = 'https://txn.xyz/v0/decode/';
 
 task(
-  'txn-dot-xyz-send', 'Generate txn.xyz URL for given transaction parameters and optionally open link in browser'
-).addOptionalParam(
-  'chainId', 'Target chain ID', undefined, types.int
-).addParam(
-  'contractAddress', 'Target address', undefined, types.string
-).addOptionalParam(
-  'fn', 'Target function name', undefined, types.string
-).addOptionalParam(
-  'fnParams', 'Target function call arguments', [], types.json
-).addFlag(
-  'browser', 'Automatically open txn.xyz URL in browser'
-).addFlag(
-  'prompt', 'Require user confirmation of successful transaction before continuing execution'
-).setAction(async function (args, hre) {
-  const url = await hre.run('txn-dot-xyz-encode', args);
+  'txn-dot-xyz-send',
+  'Generate txn.xyz URL for given transaction parameters and optionally open link in browser',
+)
+  .addOptionalParam('chainId', 'Target chain ID', undefined, types.int)
+  .addParam('contractAddress', 'Target address', undefined, types.string)
+  .addOptionalParam('fn', 'Target function name', undefined, types.string)
+  .addOptionalParam(
+    'fnParams',
+    'Target function call arguments',
+    [],
+    types.json,
+  )
+  .addFlag('browser', 'Automatically open txn.xyz URL in browser')
+  .addFlag(
+    'prompt',
+    'Require user confirmation of successful transaction before continuing execution',
+  )
+  .setAction(async function (args, hre) {
+    const url = await hre.run('txn-dot-xyz-encode', args);
 
-  console.log(`Generated txn.xyz URL: ${ url }`);
+    console.log(`Generated txn.xyz URL: ${url}`);
 
-  if (args.browser) {
-    try {
-      await open(url);
-      console.log(`Opened URL in browser.`);
-    } catch (e) {
-      throw new HardhatPluginError('failed to open txn.xyz URL in browser');
+    if (args.browser) {
+      try {
+        await open(url);
+        console.log(`Opened URL in browser.`);
+      } catch (e) {
+        throw new HardhatPluginError('failed to open txn.xyz URL in browser');
+      }
     }
-  }
 
-  if (args.prompt) {
-    console.log('Confirm pending transaction in browser.  Press enter to continue.');
+    if (args.prompt) {
+      console.log(
+        'Confirm pending transaction in browser.  Press enter to continue.',
+      );
 
-    try {
-      const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-      await new Promise((resolve) => rl.question('> ', resolve));
-      rl.close();
-    } catch (e) {
-      throw new HardhatPluginError('failed to request user input; aborting');
+      try {
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
+        });
+        await new Promise((resolve) => rl.question('> ', resolve));
+        rl.close();
+      } catch (e) {
+        throw new HardhatPluginError('failed to request user input; aborting');
+      }
     }
-  }
-});
+  });
