40370ef9b9a02e93bd0e2a2695d3b184d7ea8ed0
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-09 18:01:27 -0600

    export HardhatPlugin object

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import pkg from '../package.json';
 import './tasks/txn_dot_xyz_encode';
 import './tasks/txn_dot_xyz_send';
+import type { HardhatPlugin } from 'hardhat/types/plugins';
+
+const plugin: HardhatPlugin = {
+  id: pkg.name.split('/').pop()!,
+  npmPackage: pkg.name,
+};
