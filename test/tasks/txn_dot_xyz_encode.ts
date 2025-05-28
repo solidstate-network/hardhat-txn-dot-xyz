e3b2c01e1f8229af31f148c4e47f4435e0964050
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-27 18:42:37 -0600

    update tests

--- test/tasks/txn_dot_xyz_encode.ts
+++ test/tasks/txn_dot_xyz_encode.ts
@@ -2,54 +2,50 @@ import { TASK_TXN_DOT_XYZ_ENCODE } from '../../src/task_names.js';
 import hre from 'hardhat';
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
+import queryString from 'query-string';
 
 describe(TASK_TXN_DOT_XYZ_ENCODE, () => {
   it('returns txn.xyz transaction URL with encoded query string', async () => {
-    const chainId = 1;
+    const chainId = '0x1';
     const contractAddress = '0x' + '0'.repeat(40);
-    const fn = 'test';
+    const fnSignature = 'test()';
 
     const url = await hre.tasks.getTask(TASK_TXN_DOT_XYZ_ENCODE).run({
       chainId,
       contractAddress,
-      fn,
+      fnSignature,
     });
 
-    assert.equal(
-      url,
-      'https://txn.xyz/v0/decode/?chainID=1&contractAddress=0x0000000000000000000000000000000000000000&fn=test',
-    );
+    const parsed = queryString.parseUrl(url);
+
+    assert.equal(parsed.url, 'https://txn.xyz/v0/decode/');
+
+    assert.deepEqual(parsed.query, {
+      chainID: '1',
+      contractAddress,
+      fn: 'test',
+      abi: '[{"type":"function","name":"test","constant":false,"payable":false,"inputs":[],"outputs":[]}]',
+    });
   });
 
   it('encodes current chain id if not specified', async () => {
     const contractAddress = '0x' + '0'.repeat(40);
-    const fn = 'test';
+    const fnSignature = 'test()';
 
     const url = await hre.tasks.getTask(TASK_TXN_DOT_XYZ_ENCODE).run({
       contractAddress,
-      fn,
+      fnSignature,
     });
 
-    assert.equal(
-      url,
-      'https://txn.xyz/v0/decode/?chainID=31337&contractAddress=0x0000000000000000000000000000000000000000&fn=test',
-    );
-  });
+    const parsed = queryString.parseUrl(url);
 
-  it('encodes ABI if specified', async () => {
-    const contractAddress = '0x' + '0'.repeat(40);
-    const fn = 'test';
-    const abi: any[] = [];
+    assert.equal(parsed.url, 'https://txn.xyz/v0/decode/');
 
-    const url = await hre.tasks.getTask(TASK_TXN_DOT_XYZ_ENCODE).run({
+    assert.deepEqual(parsed.query, {
+      chainID: '31337',
       contractAddress,
-      fn,
-      abi,
+      fn: 'test',
+      abi: '[{"type":"function","name":"test","constant":false,"payable":false,"inputs":[],"outputs":[]}]',
     });
-
-    assert.equal(
-      url,
-      'https://txn.xyz/v0/decode/?abi=%5B%5D&chainID=31337&contractAddress=0x0000000000000000000000000000000000000000&fn=test',
-    );
   });
 });
