ebad0ac7f20ec36b8b362a685519dc01240332f4
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-30 18:14:39 -0600

    use hardhat-solidstate-utils dependency

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ import type { HardhatPlugin } from 'hardhat/types/plugins';
 const plugin: HardhatPlugin = {
   id: pkg.name,
   npmPackage: pkg.name,
+  dependencies: [
+    async () => {
+      const { default: HardhatSolidstateUtils } = await import(
+        '@solidstate/hardhat-solidstate-utils'
+      );
+      return HardhatSolidstateUtils;
+    },
+  ],
   tasks: [taskTxnDotXyzSend],
 };
 
