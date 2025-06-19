443a33730c4596de36be0a2208cdeac285365631
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-19 16:11:21 -0600

    run knip --production in --strict mode

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
   '*.{js,ts,sol,json,md}': ['prettier --write --no-error-on-unmatched-pattern'],
-  '*': () => ['knip', 'knip --production'],
+  '*': () => ['knip', 'knip --strict'],
 };
