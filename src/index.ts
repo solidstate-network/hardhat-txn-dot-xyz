329b50a116cb91eb227deb2fcba08e10092211c9
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-14 23:10:31 -0400

    simplify dependency plugin loading

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,7 @@ const plugin: HardhatPlugin = {
   id: pkg.name,
   npmPackage: pkg.name,
   dependencies: [
-    async () => {
-      const { default: HardhatSolidstateUtils } = await import(
-        '@solidstate/hardhat-solidstate-utils'
-      );
-      return HardhatSolidstateUtils;
-    },
+    async () => (await import('@solidstate/hardhat-solidstate-utils')).default,
   ],
   tasks: [taskTxnDotXyzSend],
 };
