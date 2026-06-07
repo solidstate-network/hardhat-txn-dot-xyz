c4899b0582ab4d8944ce752f54210bb0df00c9af
Nick Barry <itsnickbarry@protonmail.ch>
2026-06-07 16:22:34 +0900

    wrap plugin export with hardhat definePlugin

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import pkg from '../package.json' with { type: 'json' };
 import taskTxnDotXyzSend from './tasks/txn_dot_xyz_send.js';
+import { definePlugin } from 'hardhat/plugins';
 import type { HardhatPlugin } from 'hardhat/types/plugins';
 
-const plugin: HardhatPlugin = {
+const plugin: HardhatPlugin = definePlugin({
   id: pkg.name,
   npmPackage: pkg.name,
   dependencies: () => [import('@solidstate/hardhat-solidstate-utils')],
   tasks: [taskTxnDotXyzSend],
-};
+});
 
 export default plugin;
